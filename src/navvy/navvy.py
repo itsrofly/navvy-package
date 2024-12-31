20601be40dc8e0255b8a8eeb429d835066051631
itsrofly <rocketeman1234@gmail.com>
2024-12-31 04:28:47 +0000

    README Update

--- src/navvy/navvy.py
+++ src/navvy/navvy.py
@@ -3,6 +3,7 @@ import json
 from git import Repo
 from pathlib import Path
 from openai import OpenAI
+from typing import Iterator
 
 
 class Navvy:
@@ -59,22 +60,21 @@ class Navvy:
         }
     ]
 
-    def __init__(self, project_path, model="gpt-4o", system_messages=None) -> None:
+    def __init__(self, project_path, model="gpt-4o", system_messages=None, api_key=None) -> None:
         # Set the project path
-        self.project_path = Path(project_path).resolve()
-        self.repo = Repo(self.project_path)
-        self.model = model
-        self.system_messages = system_messages
-        
-        if (not self.repo.head.is_valid()):
-            initial_commit_file = self.project_path / ".gitkeep"
+        self._project_path = Path(project_path).resolve()
+        self._repo = Repo(self._project_path)
+        self._model = model
+        self._system_messages = system_messages
+
+        if (not self._repo.head.is_valid()):
+            initial_commit_file = self._project_path / ".gitkeep"
             initial_commit_file.touch()
-            self.repo.index.add([initial_commit_file])
-            self.repo.index.commit("Starting Repository")
+            self._repo.index.add([initial_commit_file])
+            self._repo.index.commit("Starting Repository")
 
-
-        if (not self.system_messages):
-            self.system_messages = [
+        if (not self._system_messages):
+            self._system_messages = [
                 {
                     "role": "system",
                     "content": """
@@ -85,20 +85,20 @@ class Navvy:
 
         # Initialize IA Chatbot
         self.client = OpenAI(
-            api_key=os.environ.get("OPENAI_API_KEY")
+            api_key=api_key if api_key else os.getenv("OPENAI_API_KEY")
         )
         self.clear_chat_history()
 
     '''Public methods'''
 
-    def send_message(self, message):
+    def send_message(self, message) -> Iterator[str]:
         # Get clean chat history
         chat = self.get_chat_history()
 
         # Add files from repo to the chat
         # Don't save directly to the chat history because the files can change and it's more economical.
         file_contents = self.__get_all_file_contents(
-            self.repo.head.commit.tree)
+            self._repo.head.commit.tree)
         if not file_contents:
             file_contents = "No files in the repository."
 
@@ -110,7 +110,7 @@ class Navvy:
         # Add all commits for context
         chat.append({
             "role": "system",
-            "content": "Last three commits:" + str(list(self.repo.iter_commits()))
+            "content": "Last three commits:" + str(list(self._repo.iter_commits()))
         })
 
         chat.append({
@@ -118,7 +118,7 @@ class Navvy:
             "content": message
         })
 
-        self.chat_history.append({
+        self._chat_history.append({
             "role": "user",
             "content": message
         })
@@ -128,15 +128,17 @@ class Navvy:
 
         response = self.client.chat.completions.create(
             messages=chat,
-            model=self.model,
+            model=self._model,
             tools=self.__tools,
             stream=True
         )
 
         for chunk in response:
             delta = chunk.choices[0].delta
+
             if delta.content:
                 yield delta.content
+
             elif delta.tool_calls:
                 tool_call = delta.tool_calls[0]
                 function = tool_call.function
@@ -151,6 +153,7 @@ class Navvy:
                     functions[tool_call.index]["name"] = function.name
                 if function.arguments:
                     functions[tool_call.index]["arguments"] += function.arguments
+
                 yield function.arguments
 
         for function in functions:
@@ -163,28 +166,28 @@ class Navvy:
                 self.__delete_file(**function_arguments)
 
     def get_chat_history(self):
-        return self.chat_history.copy()
+        return self._chat_history.copy()
 
     def clear_chat_history(self) -> None:
         # Add system messages to the chat history
-        self.chat_history = self.system_messages.copy()
+        self._chat_history = self._system_messages.copy()
 
-    def undo_commit_changes(self, commit_id=None):
+    def undo_commit_changes(self, commit_id=None) -> None:
         if (not commit_id):
-            last_commits = list(self.repo.iter_commits(max_count=2))
+            last_commits = list(self._repo.iter_commits(max_count=2))
             last_element = last_commits[-1]
             commit_id = last_element.hexsha
         # Undo Commit
-        self.repo.git.execute(["git", "reset", commit_id])
+        self._repo.git.execute(["git", "reset", commit_id])
 
         # Discard all changes files in the project path
-        self.repo.git.execute(
-            ["git", "checkout", str(self.project_path / ".")])
-        self.repo.git.execute(["git", "clean", "-fdx"])
+        self._repo.git.execute(
+            ["git", "checkout", str(self._project_path / ".")])
+        self._repo.git.execute(["git", "clean", "-fdx"])
 
     def get_all_commits_ids(self):
         # Get all commit ids (hexsha) & commit messages
-        return [(commit.hexsha, commit.message) for commit in self.repo.iter_commits()]
+        return [(commit.hexsha, commit.message) for commit in self._repo.iter_commits()]
 
     '''Private methods'''
 
@@ -195,7 +198,7 @@ class Navvy:
                 file_path = entry.path
                 try:
                     # Read and add file content to contents list
-                    with open(self.project_path / file_path, 'r', encoding='utf-8') as f:
+                    with open(self._project_path / file_path, 'r', encoding='utf-8') as f:
                         file_content = f.read()
                         contents.append(f'File:{file_path}\n{file_content}\n')
                 except Exception as e:
@@ -204,7 +207,7 @@ class Navvy:
 
     def __edit_file(self, file_path, file_content, commit_message):
         # Ensure the directory exists
-        complete_path = self.project_path / file_path
+        complete_path = self._project_path / file_path
         os.makedirs(complete_path.parent, exist_ok=True)
 
         # Write the new content to the file
@@ -212,20 +215,20 @@ class Navvy:
             f.write(file_content)
 
         # Add the file to the index
-        self.repo.index.add([file_path])
+        self._repo.index.add([file_path])
 
         # Commit the changes
-        self.repo.index.commit(commit_message)
+        self._repo.index.commit(commit_message)
 
     def __delete_file(self, file_path, commit_message):
         # Delete the file
-        complete_path = (self.project_path / file_path)
+        complete_path = (self._project_path / file_path)
 
         # Check if the file exists before attempting to delete
         if complete_path.exists():
             # Delete the file
             os.remove(complete_path)
 
-        self.repo.git.add(update=True)
+        self._repo.git.add(update=True)
         # Commit the changes
-        self.repo.index.commit(commit_message)
+        self._repo.index.commit(commit_message)
